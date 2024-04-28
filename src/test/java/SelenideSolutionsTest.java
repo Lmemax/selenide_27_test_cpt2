58985d782c21865b9722245da6d40b9883f0de0d
Max Iudin <LmeMyu@yandex.ru>
2024-04-28 23:26:24 +0300

    added estSolutionsEnterprise

---
+++ src/test/java/SelenideSolutionsTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideSolutionsTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void testSolutionsEnterprise() {
+
+        open("");
+        $(byTagAndText("button","Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
+    }
+}
